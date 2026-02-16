be1d8d6429bb09ff110c300a785dc437c4a91064
wasp32679 <ryan.kenmoe@outlook.fr>
2026-02-16 11:23:55 +0100

    chore: get request on /todos close #3

--- index.ts
+++ index.ts
@@ -1,9 +1,14 @@
+import db from './db';
 import index from './index.html';
 
 const server = Bun.serve({
   port: 3000,
   routes: {
     '/': index,
+    '/todos': () => {
+      const data = db.query('select * from todos').all();
+      return Response.json(data);
+    },
   },
 });
 
